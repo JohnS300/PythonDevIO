ccd3c6344ed4ac29d1ca50b5359a834f4644c374
JohnS300 <johnsofokleous@gmail.com>
2025-05-20 23:26:04 +0300

    New Class for the Expense with a decorator that inspects the attributes declared and injects an __init__, __repr__, __eq__, and other methods; List to hold all Expense Objects; Function that appends each new expense to a list; Function that returns a copy of the list;

---
+++ third_app/src/expense_tracker_v2/main.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from dataclasses import dataclass
+from datetime import date
+
+
+_expenses = []
+
+
+class Category(Enum):
+    INVESTMENT = 'investment'
+    HOBBY = 'hobby'
+    GIRLFRIEND = 'girlfriend'
+    RENT = 'rent'
+    GROCERIES = 'groceries'
+    ELECTRICITY = 'electricity'
+    PHONE = 'phone'
+    ENTERTAINMENT = 'entertainment'
+
+
+@dataclass
+class Expense:
+    amount: float
+    date_of_expense: date
+    category: Category
+    description: str
+
+
+def add_expenses(amount, date_of_expense, category, description):
+    expense = Expense(amount, date_of_expense, category, description)
+    _expenses.append(expense)
+
+
+def list_expenses():
+    return list(_expenses)
