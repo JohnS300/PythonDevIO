f31e862679db7c8b2fdc2f4cd6f0fdcce4d4f52f
JohnS300 <johnsofokleous@gmail.com>
2025-05-30 23:55:57 +0300

    Fixing and multiple changes on Testing

--- third_app/tests/test_summary.py
+++ third_app/tests/test_summary.py
@@ -1,20 +1,25 @@
 import pytest
-
-from expense_tracker_v2.main import total_expenses, filter_by_category, remove_expense, clear_expenses, _expenses
+from datetime import date
+from expense_tracker_v2.main import total_expenses, filter_by_category, remove_expense, clear_expenses, _expenses, add_expenses, Category
 
 
 def setup_function():
     _expenses.clear()
 
 
-def test_invalid_total_expenses_call():
-    
+def test_get_valid_total_expenses():
+    add_expenses(10, date(2025, 5, 25), Category.GIRLFRIEND, 'Coffe')
+    add_expenses(20.5, date(2025, 5, 25), Category.GIRLFRIEND, 'Brunch')
+    add_expenses(5, date(2025, 5, 25), Category.GIRLFRIEND, 'Kiosk')
+    assert len(_expenses) == 3
+    assert total_expenses() == 35.5
 
 
-@pytest.mark.parametrize("bad_category_filter", ['', ' ', 'Tomato', 'None'], ids=['empty', 'spaces', 'string', None])
-def test_filter_by_category(bad_category_filter):
-    with pytest.raises(TypeError):
-        filter_by_category(bad_category_filter)
+@pytest.mark.parametrize("category_filter", ['', ' ', 'Tomato', 'None'], ids=['empty', 'spaces', 'string', None])
+def test_filter_by_category(category_filter):
+    filter_by_category(category_filter)
 
 
-def test_invalid_total_expenses_call():
+def invalid_clear_expenses():
+    _expenses.clear()
+    # somehow test what we get when calling the clear function
